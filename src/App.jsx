dfbe35fdeff142a69844be046bcbfe8df5458698
Ifeanyi Osuji <oindubuisi@gmail.com>
2023-09-02 18:15:31 +0100

    Changed promise call to async await in the useeffect function, created a custome hook

--- src/App.jsx
+++ src/App.jsx
@@ -19,46 +19,8 @@ export default function App() {
       </div>
     );
   }
-// const products = [
-//   {
-//     "id": 1,
-//     "category": "shoes",
-//     "image": "shoe1.jpg",
-//     "name": "Hiker",
-//     "price": 94.95,
-//     "skus": [
-//       { "sku": "17", "size": 7 },
-//       { "sku": "18", "size": 8 }
-//     ],
-//     "description": "This rugged boot will get you up the mountain safely."
-//   },
-//   {
-//     "id": 2,
-//     "category": "shoes",
-//     "image": "shoe2.jpg",
-//     "name": "Climber",
-//     "price": 78.99,
-//     "skus": [
-//       { "sku": "28", "size": 8 },
-//       { "sku": "29", "size": 9 }
-//     ],
-//     "description": "Sure-footed traction in slippery conditions."
-//   },
-//   {
-//     "id": 3,
-//     "category": "shoes",
-//     "image": "shoe3.jpg",
-//     "name": "Explorer",
-//     "price": 145.95,
-//     "skus": [
-//       { "sku": "37", "size": 7 },
-//       { "sku": "38", "size": 8 },
-//       { "sku": "39", "size": 9 }
-//     ],
-//     "description": "Look stylish while stomping in the mud."
-//   }
-// ]
-    const {data: products, loading, error} = useProductState()
+
+    const {data: products, loading, error} = useProductState("products?category=shoes")
     
   
   const filteredProduct = size ? products.filter(({skus}) => skus.find(sku => sku.size === parseInt(size))) : products
--- src/services/useProductState.js
+++ src/services/useProductState.js
@@ -1,20 +1,34 @@
 import { useEffect, useState } from "react"
 import { getProducts } from "./productService";
+const baseUrl = process.env.REACT_APP_API_BASE_URL;
 
 
-const useProductState = () =>{
+const useProductState = (url) =>{
 const [data, setData] = useState([]);
 const [loading, setLoading] = useState(true);
 const [error, setError] = useState(null)
 
 useEffect(()=>{
-     console.log('in use effect')
-   getProducts('shoes').then(response =>
-    setData(response)).catch(e => {setLoading(false)
-    setError(e);
-}).finally(()=>setLoading(false));
-   
-}, []);
+     async function init (){
+        try{
+        const response = await fetch(baseUrl + url)
+        //console.log(response.json())
+        if(response.ok){
+            const json = await response.json();
+        setData(json);
+        
+        }
+        else{setError(response)}
+        
+        }
+        catch(e){
+            setError(e);
+        }
+        finally{setLoading(false)}
+     }
+     init();
+  
+}, [url]);
 return {data, loading, error}
 }
 
