5769ada4b9a66cf447241f7fe597378beef781a0
Ifeanyi Osuji <oindubuisi@gmail.com>
2023-09-05 12:52:57 +0100

    Implemented adding shoes to cart, updating cart, saving cart to local storage and checking out of cart

--- src/Products.jsx
+++ src/Products.jsx
@@ -3,34 +3,29 @@ import "./App.css";
 
 import useProductState from "./services/useProductState";
 import Spinner from "./Spinner";
-import { NavLink, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import PageNotFound from "./PageNotFound";
+//import ProductProvider from "./context/ProductsContext";
+import RenderProduct from "./RenderedProducts";
 
 
 export default function Products() {
   const [size, setSize] = useState("");
-  function renderProduct(p) {
-    return (
-      
-      <div key={p.id} className="product">
-         <NavLink to= {`/${category}/${p.id}`}>
-          <img src={`/images/${p.image}`} alt={p.name} />
-          <h3>{p.name}</h3>
-          <p>${p.price}</p>
-          </NavLink>
-      </div>
-     
-    );
-  }
+
+
+
   const {category} = useParams();
-    const {data: products, loading, error} = useProductState(`products?category=${category}`)
-    
+    const {data: products, loading, error} = useProductState(`products?category=${category}`);
+    // let sizes = new Set();
+    // products.map(product=>product.skus.map(sku=>{
+    //     sizes.add(sku.size)
+    // }))
   
-  const filteredProduct = size ? products.filter(({skus}) => skus.find(sku => sku.size === parseInt(size))) : products
+  const filteredProduct = size ? products.filter(({skus}) => skus.find(sku => sku.size === parseInt(size))) : products;
   if(error) throw error;
   if(products.length ===0)return <PageNotFound />
   return (
-    <>
+   
       <div className="content">
           <section id="filters">
             <label htmlFor="size">Filter by Size:</label>{" "}
@@ -44,9 +39,9 @@ export default function Products() {
          
           <section id="products">
             
-          {!loading ? filteredProduct.map(renderProduct): <Spinner />}
+          {!loading ? filteredProduct.map(RenderProduct): <Spinner />}
           </section>
             </div>
-    </>
+   
   );
 }
---
+++ src/services/useFetchAll.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react"
+
+const baseUrl = process.env.REACT_APP_API_BASE_URL;
+
+
+ export default function useFetchAll(urls){
+    
+
+const [data, setData] = useState(null);
+const [loading, setLoading] = useState(true);
+const [error, setError] = useState(null)
+
+useEffect(() => {
+    const promises = urls.map((url) =>
+      fetch(baseUrl + url).then((response) => {
+        if (response.ok) return response.json();
+        console.log('url',url);
+        throw response;
+      })
+    );
+
+    Promise.all(promises)
+      .then((json) => setData(json))
+      .catch((e) => {
+        console.error(e);
+        setError(e);
+      })
+      .finally(() => setLoading(false));
+    // eslint-disable-next-line
+  }, []);
+console.log('data=> ',data)
+return {data, loading, error}
+}
+
